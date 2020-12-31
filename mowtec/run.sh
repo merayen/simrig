774085c8dcaaf971c53d8f7da748476763be8537
Merayen Salberg <merayen@pi.local>
2020-12-31 12:02:38 +0000

    Opens GPIO memory and mmap it

--- mowtec/run.sh
+++ mowtec/run.sh
@@ -1,1 +1,1 @@
-cargo build
+cargo run
--- mowtec/src/main.rs
+++ mowtec/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+	// https://doc.rust-lang.org/std/ffi/struct.CString.html#method.as_ptr
+	let path = std::ffi::CString::new("/dev/gpiomem").unwrap();
+	unsafe {
+		let mem_fd = libc::open(path.as_ptr(), libc::O_RDWR | libc::O_SYNC | libc::O_CLOEXEC);
+		if mem_fd < 0 {
+			panic!("Could not open /dev/gpiomem");
+		}
+
+		let gpio_map = libc::mmap(
+			std::ptr::null_mut(),
+			4 * 1024,
+			libc::PROT_READ | libc::PROT_WRITE,
+			libc::MAP_SHARED,
+			mem_fd,
+			0x200000
+			//(0xFE000000i64 + 0x200000i64) as i32
+		);
+
+		libc::close(mem_fd);
+
+		if gpio_map == libc::MAP_FAILED {
+			panic!("Could not mmap GPIO");
+		}
+	}
 }
