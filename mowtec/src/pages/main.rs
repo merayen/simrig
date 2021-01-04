6fd2a0bec3a9b2c9387c2057e023e304657682f4
Einar Rosvoll <pigzelner@gmail.com>
2021-01-04 21:14:10 +0000

    Buggy, but not that buggy

--- mowtec/src/pages/main.rs
+++ mowtec/src/pages/main.rs
@@ -10,21 +10,29 @@ pub struct Main {
 impl Page for Main {
 	fn draw(&mut self) -> Option<&mut Draw>{
 		// Gear
+		self.draw.bg_current = 3;
 		self.draw.frame(35, 10, 30, 10);
 		get_7_segment_text(43, 11, 12, 9, ((get_time() / 1000) % 12) as usize, &mut self.draw);
 
 		// Clutch
-		self.draw.bg_current = 3;
+		self.draw.bg_current = 9;
 		self.draw.frame(70, 10, 30, 3);
-		self.draw.bg_current = 15;
+		self.draw.bg_current = 0;
+		self.draw.fg_current = 15;
 		self.draw.text(71, 11, "Clutch");
 
 		// Brake
+		self.draw.bg_current = 9;
 		self.draw.frame(70, 13, 30, 3);
+		self.draw.bg_current = 0;
+		self.draw.fg_current = 15;
 		self.draw.text(71, 14, "Brake");
 
 		// Throttle
+		self.draw.bg_current = 9;
 		self.draw.frame(70, 16, 30, 3);
+		self.draw.bg_current = 0;
+		self.draw.fg_current = 15;
 		self.draw.text(71, 17, "Throttle");
 
 		Some(&mut self.draw)
