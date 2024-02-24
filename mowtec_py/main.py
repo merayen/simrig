1fbcc1708d048df285e93da7ebba94bc914ed48d
Einar Rosvoll <einar.rosvoll@gmail.com>
2024-02-24 16:22:06 +0100

    RPM gauge

--- mowtec_py/main.py
+++ mowtec_py/main.py
@@ -9,18 +9,45 @@ WIDTH = 720
 HEIGHT = 512
 
 pygame.init()
+pygame.font.init()
 
 running = True
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 clock = pygame.time.Clock()
 
-def pos(x,y):
-	return (x*WIDTH, y*HEIGHT)
+lolnumber = 500
+
+
+def pos(x,y=None):
+	if y is not None:
+		return (x*WIDTH, y*HEIGHT)
+	return x*((WIDTH+HEIGHT)/2)
+
+
+def font_size(a):
+	return round(a*((WIDTH+HEIGHT)/2) / lolnumber)
+
+def size(x):
+	return round(x*((WIDTH+HEIGHT)/2) / lolnumber)
+
+
+rpm_title = pygame.font.SysFont("Arial", font_size(12)).render("rpm", True, (127,100,0), (0,0,0))
 
 
 def draw_rpm_bar(n, n_max):
-	pygame.draw.rect(screen, "grey", (*pos(.01,.01,), *pos(.98,.1)), width=1)
-	pygame.draw.rect(screen, (127,100,0), (*pos(.02,.05,), *pos(.96,.05)), width=1000)
+	# RPM lines
+	rpm_digit = pygame.font.SysFont("Arial", font_size(12))
+	for i in range(n_max):
+		v = 1.1*(i/n_max) + .02
+		pygame.draw.line(screen, "grey", [*pos(v, .05)], [*pos(v, .1)], True)
+		screen.blit(rpm_digit.render(str(i+1), True, (127,100,0), (0,0,0)), pos(v - .005, .05))
+
+	pygame.draw.rect(screen, "grey", (*pos(.01,.01,), *pos(.98, .1)), width=size(1))
+
+	# RPM moving bar
+	pygame.draw.rect(screen, (127,100,0), (*pos(.02,.05,), *pos(1.1*(max(0, min(n, n_max)-1)/n_max),.05)), width=1000)
+
+	screen.blit(rpm_title, pos(.5, .01))
 
 
 while running:
@@ -31,7 +58,7 @@ while running:
 
 	screen.fill('black')
 	pygame.draw.rect(screen, "white", (*pos(0,0), *pos(1, 1)), width=1)
-	draw_rpm_bar(6.8, 8)
+	draw_rpm_bar(4, 8)
 
 	pygame.display.flip()
 	clock.tick(60)
